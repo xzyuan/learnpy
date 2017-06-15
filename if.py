c6d763a3bbc3b10ac2099871ef5051946c38b727
xzyuan <xzyuan@mail.ustc.edu.cn>
2017-06-15 16:03:30 +0800

    if learning_3

---
+++ if.py
@@ -0,0 +1,38 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+# for bicycle in bicycles:
+#     if bicycle == 'cannondale':
+#         print(bicycle.upper())
+#     else:
+#         print(bicycle)
+
+# print("a" == "A")
+
+#检查多个条件
+#  condition_1 and condition_2
+
+#检查列表中特定值
+# print('trek' in bicycles)
+
+# age = 12
+#
+# if age<4:
+#     price = 0
+# elif age <18:
+#     price = 5
+# else:
+#     price = 10  #也可省略else，如加入剩余条件
+#
+# print(price)
+
+# practice
+# alien_color = ['green', 'yellow', 'red']
+# if 'green' in alien_color:
+#     print("You got 5 points")
+
+#确定列表非空
+# if bicycles:
+#     print(1)
+
+
+
