2cba6eca62974586402e11926351a9398b6ca0e3
xzyuan <xzyuan@mail.ustc.edu.cn>
2017-06-15 14:44:55 +0800

    list learning_2

---
+++ operate_list.py
@@ -0,0 +1,48 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+#遍历
+# for bicycle in bicycles:
+#     print(bicycle)
+
+#数值列表
+# for value in range(1,5):
+#      print(value)
+# # 1
+# # 2
+# # 3
+# # 4
+
+# numbers = list(range(1,6))
+# print(numbers)
+#
+# numbers_1 = list(range(1,11,2))
+# print(numbers_1)
+
+# squares = []
+# for value in range(1,11,2):
+#     squares.append(value**3)
+# print(squares)
+
+#统计计算
+# numbers = []
+# min(numbers)
+# max(numbers)
+# sum(numbers)
+
+#列表解析
+# squares = [value**2 for value in range(1,5)]
+# print(squares)
+
+
+# practice
+# 1
+# for a in range(1,21):
+#     print(a)
+
+# 2
+# odd = list(range(1,20,2))
+# print(odd)
+
+#3
+# cube = [value**3 for value in range(1,11)]
+# print(cube)
