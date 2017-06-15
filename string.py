25fc99a5cb1d546ed3608748f30dd6de8480677b
xzyuan <xzyuan@mail.ustc.edu.cn>
2017-06-15 11:54:07 +0800

    string learning

---
+++ string.py
@@ -0,0 +1,23 @@
+# name = "Ada lovelace"
+# print(name.title())
+
+# name = "Ada lovelace"
+# print(name.upper())
+# print(name.lower())
+
+# first_name = "ada"
+# last_name = "lovelace"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+# print("Python")
+# print("\tPython")
+# print("python\nPython")
+
+# word = 'python '
+# print(word)
+# print(word.rstrip())  #暂时删除末尾空白
+# word = word.rstrip()
+# word.lstrip()   #开头空白
+# word.strip()    #两端空白
+
