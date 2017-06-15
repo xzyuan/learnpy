36d0aaaa6b0e5641719349f912150857f005ff16
xzyuan <xzyuan@mail.ustc.edu.cn>
2017-06-15 17:31:16 +0800

    input and cycle  learning

---
+++ input_and_cycle.py
@@ -0,0 +1,28 @@
+# message = input("hello \nplease inupt a number:")
+#
+# if int(message) >= 18:
+#     print(message)
+
+#模
+# print(4%3)
+
+#while循环
+# current_number = 0
+# while current_number <=5:
+#     current_number += 1
+#     print(current_number)
+
+# message = "blanck"
+# while message != "quit":
+#     message = input()
+#     print("6")
+
+#使用break退出循环
+#continue进入下次循环
+
+# users = ['asd', 'asdas', 'fdfdf']
+# users_1 = []
+# while users:
+#     users_1.insert(0,users.pop())
+# print(users_1)
+# print(users)
