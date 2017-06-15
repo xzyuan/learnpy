382da7708f0665ea6b4c36d78d62fbc655f84d50
xzyuan <xzyuan@mail.ustc.edu.cn>
2017-06-15 22:32:57 +0800

    files learning

---
+++ json1.py
@@ -0,0 +1,17 @@
+#json.dump() & json.load()
+
+import json
+
+numbers = list(range(1,5))
+# print(numbers)
+filename = 'numbers.json'
+# with open(filename,'w') as file_object:
+#     json.dump(numbers, file_object)
+
+
+
+# with open(filename) as file_object:
+#     numbers = json.load(file_object)
+#
+# print(numbers)
+
