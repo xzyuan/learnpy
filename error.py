f86b4c572c2717d3023373193282b2caa5da2f60
xzyuan <xzyuan@mail.ustc.edu.cn>
2017-06-15 22:10:07 +0800

    files learning

---
+++ error.py
@@ -0,0 +1,19 @@
+#异常
+#try-expect代码块处理
+#ZeroDivisionError异常
+# print(5/0)
+
+# try:
+#     print(5/0)
+# except ZeroDivisionError:
+#     print("can't divive by 0")
+
+#else代码块,执行try成功了之后的操作
+# try:
+#
+# except:
+#
+# else:
+
+#FileNotFoundError异常
+
